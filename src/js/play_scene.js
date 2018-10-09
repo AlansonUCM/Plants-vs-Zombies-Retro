44fc05b8323b3f6b073693626fbddbe43e8208eb
DFaouaz <dfaouaz@ucm.es>
2018-10-09 17:46:26 +0200

    Primeras implementaciones
    Primeras implementaciones de las clases

--- src/js/play_scene.js
+++ src/js/play_scene.js
@@ -8,4 +8,42 @@
   }
 };
 
+//CLASE PLANT
+function Plant (name, life){
+  Sprite.apply(this,[]);
+  this._name = name;
+  this._life = life;
+}
+Plant.prototype = Object.create(Sprite.prototype);
+Plant.prototype.constructor = Plant;
+
+//Ejemplo LanzaGuisantes
+function LanzaGuisantes(name, life){
+  Plant.apply(this,[name, life]);
+  //Atributos propios
+  //----------------
+  //----------------
+}
+LanzaGuisantes.prototype = Object.create(Plant.prototype);
+LanzaGuisantes.prototype.constructor = LanzaGuisantes;
+
+//CLASE ZOMBIE
+function Zombie (name, life){
+  Sprite.apply(this,[]);
+  this._name = name;
+  this._life = life;
+}
+Zombie.prototype = Object.create(Sprite.prototype);
+Zombie.prototype.constructor = Plant;
+
+//Ejemplo ZombieComun
+function ZombieComun(name, life){
+  Zombie.apply(this,[name, life]);
+  //Atributos propios
+  //----------------
+  //----------------
+}
+ZombieComun.prototype = Object.create(Zombie.prototype);
+ZombieComun.prototype.constructor = ZombieComun;
+
 module.exports = PlayScene;
